93edfeb0429d64d4cbfe339438b3b72183d1cd2b
chenxiaohua <3389162133@qq.com>
2023-11-25 11:16:13 +0800

    refactor🌟(UserStore): refine the type of currentUser and adjust the assignment in fetchCurrentUser
    The type of currentUser in UserStore was previously any, which is not specific enough. Now it has been refined to User. In addition, the assignment in fetchCurrentUser has been adjusted to correctly assign the data field of the response to currentUser.

--- src/store/modules/UserStore.ts
+++ src/store/modules/UserStore.ts
@@ -3,13 +3,14 @@ import type { Ref } from 'vue';
 import { ref } from 'vue';
 
 import { getCurrentUser } from '@/api';
+import type { User } from '@/models';
 
 export const useUserStore = defineStore(
   'user',
   () => {
-    const currentUser = <Ref<any | null>>ref(null);
+    const currentUser = <Ref<User | null>>ref(null);
     const fetchCurrentUser = async () => {
-      currentUser.value = await getCurrentUser();
+      currentUser.value = (await getCurrentUser()).data;
     };
     const reset = () => {
       currentUser.value = null;
